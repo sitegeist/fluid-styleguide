f2bb6168b10b50041c718f31d42ae46af2bed049
Bastian Schwarz <schwarz@infomax-it.de>
2022-01-26 15:33:15 +0100

    Removed the infomax license
    Signed-off-by: Bastian Schwarz <schwarz@infomax-it.de>

--- Classes/Factory/Component/ComponentFactoryInterface.php
+++ Classes/Factory/Component/ComponentFactoryInterface.php
@@ -1,14 +1,4 @@
 <?php declare(strict_types=1);
-/**
- * LICENSE
- *
- * This software and its source code is protected by copyright law (Sec. 69a ff. UrhG).
- * It is not allowed to make any kinds of modifications, nor must it be copied,
- * or published without explicit permission. Misuse will lead to persecution.
- *
- * @copyright  2022 infomax websolutions GmbH
- * @link       http://www.infomax-it.de
- */
 
 namespace Sitegeist\FluidStyleguide\Factory\Component;
 
