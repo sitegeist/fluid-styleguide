78ca90368d9ac3dbe402cfbddbcf1679b0626abf
Ulrich Mathes <mathes@sitegeist.de>
2024-09-24 18:22:05 +0200

    [v13] do not use renderStatic for fluid 4

--- Classes/ViewHelpers/Format/MarkdownViewHelper.php
+++ Classes/ViewHelpers/Format/MarkdownViewHelper.php
@@ -3,14 +3,9 @@ declare(strict_types=1);
 
 namespace Sitegeist\FluidStyleguide\ViewHelpers\Format;
 
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
-
 class MarkdownViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
     protected static \Parsedown $markdownParser;
 
     /**
@@ -26,15 +21,11 @@ class MarkdownViewHelper extends AbstractViewHelper
     /**
      * Renders markdown code in fluid templates
      */
-    public static function renderStatic(
-        array $arguments,
-        \Closure $renderChildrenClosure,
-        RenderingContextInterface $renderingContext
-    ): string {
+    public function render(): string
+    {
         if (!isset(static::$markdownParser)) {
             static::$markdownParser = new \Parsedown();
         }
-
-        return static::$markdownParser->text($renderChildrenClosure());
+        return static::$markdownParser->text($this->renderChildren());
     }
 }
